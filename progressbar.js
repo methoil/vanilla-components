0d4416394d936a1096d538473b6883b3cb704569
Boris Mateev <mateev.br@gmail.com>
2020-04-29 23:17:21 -0400

    feat - controls working properly

--- progressbar.js
+++ progressbar.js
@@ -1,26 +1,47 @@
 // make the progress bar load over 5 seconds
+const max = 100;
+const progressWidth = 300;
+const increment = 3;
+let count = 0;
+
+let greenBar;
+let redBar;
+let percent;
+let inputTextBox;
+
+function setup() {
+  greenBar = document.getElementById("green-bar");
+  redBar = document.getElementById("red-bar");
+  percent = document.getElementById("status-number");
+  inputTextBox = document.getElementById("input-text-box");
+}
+
+function onInputChange(event) {
+  if (event && event.keyCode === 13) {
+    startLoading();
+  }
+}
 
 function startLoading() {
-  let count = 0;
-  const max = 100;
-  const increment = 3;
-
-  const greenBar = document.getElementById("green-bar");
-  const redBar = document.getElementById("red-bar");
-  const percent = document.getElementById("status-number");
-
+  const input = inputTextBox.value.replace(/\D/g, "");
+  if (!input) {
+    return;
+  }
+  const time = (parseInt(input) * 1000) / max;
+  count = 0;
   setLoading();
 
   function setLoading() {
     greenBar.style.width = `${increment * count}px`;
-    redBar.style.width = `${300 - increment * count}px`;
+    redBar.style.width = `${progressWidth - increment * count}px`;
     percent.innerHTML = `${count}%`;
     count++;
 
+    // setInterval is a thing....
     setTimeout(() => {
       if (count <= max) {
         setLoading();
       }
-    }, 50);
+    }, time);
   }
 }
