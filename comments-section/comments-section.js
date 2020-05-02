eb5f27e65fbbd448e761f7b8961b555a92e57ca7
Boris Mateev <mateev.br@gmail.com>
2020-05-01 20:57:19 -0400

    feat(comment-container): existing comment container + buttons ui

--- comments-section/comments-section.js
+++ comments-section/comments-section.js
@@ -1,8 +1,8 @@
-let commentsContainer;
+let allCommentsContainer;
 let addRootCommentButton;
 
 function setup() {
-  commentsContainer = document.getElementById("comments-container");
+  allCommentsContainer = document.getElementById("comments-container");
   addRootCommentButton = document.getElementById("add-root-comment-button");
 }
 
@@ -22,17 +22,45 @@ function addRootComment(event) {
   newCommentInputContainer.appendChild(newCommentInput);
   newCommentInputContainer.appendChild(newCommentButton);
 
-  commentsContainer.appendChild(newCommentInputContainer);
+  allCommentsContainer.appendChild(newCommentInputContainer);
 }
 
 function submitComment(event) {
   const text = event.target.parentElement.getElementsByClassName(
     "comment-input-textbox"
   )[0].value;
+
   const newComment = document.createElement("div");
   newComment.innerHTML = text;
   newComment.classList.add("comment-text");
 
-  commentsContainer.appendChild(newComment);
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("comment-button-container");
+
+  const replyButton = document.createElement("input");
+  replyButton.type = "button";
+  replyButton.value = "Reply";
+  replyButton.addEventListener("click", () => {}); // TODO: reply CB
+
+  const removeButton = document.createElement("input");
+  removeButton.type = "button";
+  removeButton.value = "Remove Comment";
+  removeButton.addEventListener("click", () => {}); // TODO: remove CB
+
+  const editButton = document.createElement("input");
+  editButton.type = "button";
+  editButton.value = "Edit Comment";
+  editButton.addEventListener("click", () => {}); // TODO: edit CB
+
+  buttonContainer.appendChild(removeButton);
+  buttonContainer.appendChild(editButton);
+  buttonContainer.appendChild(replyButton);
+
+  const newCommentContainer = document.createElement("div");
+  newCommentContainer.classList.add("comment-container");
+  newCommentContainer.appendChild(newComment);
+  newCommentContainer.appendChild(buttonContainer);
+
+  allCommentsContainer.appendChild(newCommentContainer);
   event.target.parentElement.remove();
 }
